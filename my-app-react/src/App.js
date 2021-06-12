696aa6321c532b2d422e2c4b86787e02b2c68460
David <jlhuangch@gmail.com>
2021-06-12 14:32:17 +0800

    pdf

--- my-app-react/src/App.js
+++ my-app-react/src/App.js
@@ -1,4 +1,6 @@
 import { Input, Button } from "antd";
+import jsPDF from "jspdf";
+import html2canvas from "html2canvas";
 import "./App.css";
 import { Component } from "react";
 import ajax from "./utils/ajaxUtil";
@@ -12,7 +14,7 @@ class App extends Component {
       data: [],
       studentId: "",
       average: [],
-      single: []
+      single: [],
     };
   }
   componentDidMount() {
@@ -28,13 +30,13 @@ class App extends Component {
           top: "bottom",
         },
         toolbox: {
-          show: true,
-          feature: {
-            mark: { show: true },
-            dataView: { show: true, readOnly: false },
-            restore: { show: true },
-            saveAsImage: { show: true },
-          },
+          // show: true,
+          // feature: {
+          //   mark: { show: true },
+          //   dataView: { show: true, readOnly: false },
+          //   restore: { show: true },
+          //   saveAsImage: { show: true },
+          // },
         },
         series: [
           {
@@ -67,10 +69,8 @@ class App extends Component {
     });
     studentRes.then((value) => {
       this.setState({
-        average: value.data
-      })
-      console.log(this.state.average)
-      console.log(value);
+        average: value.data,
+      });
       var allec = echarts.init(document.getElementById("allStudentEcharts"));
       allec.setOption({
         title: {
@@ -104,85 +104,129 @@ class App extends Component {
           {
             name: "总体",
             type: "bar",
-            data: [this.state.average.yuwen_4, this.state.average.yuwen_3, this.state.average.yuwen_2, this.state.average.yuwen_1,this.state.average.sum],
+            data: [
+              this.state.average.yuwen_4,
+              this.state.average.yuwen_3,
+              this.state.average.yuwen_2,
+              this.state.average.yuwen_1,
+              this.state.average.sum,
+            ],
           },
           {
             name: "个人",
             type: "bar",
-            data: [this.state.single.yuwen_4, this.state.single.yuwen_3, this.state.single.yuwen2, this.state.single.yuwen_1,this.state.single.sum],
+            data: [
+              this.state.single.yuwen_4,
+              this.state.single.yuwen_3,
+              this.state.single.yuwen2,
+              this.state.single.yuwen_1,
+              this.state.single.sum,
+            ],
           },
         ],
       });
     });
-    console.log(this);
   }
 
   onSub = (value) => {
-    console.log(value.target);
-    var singleRes = ajax("/api/single/" + this.state.studentId, {}, "GET")
-    singleRes.then(value => {
+    var singleRes = ajax("/api/single/" + this.state.studentId, {}, "GET");
+    singleRes.then((value) => {
       if (value.data.info === "not found") {
-        console.log("not found")
+        console.log("not found");
         this.setState({
-          info: "student not found"
-        })
+          info: "student not found",
+        });
       } else {
         this.setState({
           single: value.data,
-          info: ""
-        })
+          info: "",
+        });
         var allec = echarts.init(document.getElementById("allStudentEcharts"));
-      allec.setOption({
-        title: {
-          text: "语文成绩",
-          subtext: "数据来自xlsx",
-        },
-        tooltip: {
-          trigger: "axis",
-          axisPointer: {
-            type: "shadow",
+        allec.setOption({
+          title: {
+            text: "语文成绩",
+            subtext: "数据来自xlsx",
           },
-        },
-        legend: {
-          data: ["总体", "个人"],
-        },
-        grid: {
-          left: "3%",
-          right: "4%",
-          bottom: "3%",
-          containLabel: true,
-        },
-        xAxis: {
-          type: "value",
-          boundaryGap: [0, 0.01],
-        },
-        yAxis: {
-          type: "category",
-          data: ["yuwen_4", "yuwen_3", "yuwen_2", "yuwen_1", "总分"],
-        },
-        series: [
-          {
-            name: "总体",
-            type: "bar",
-            data: [this.state.average.yuwen_4, this.state.average.yuwen_3, this.state.average.yuwen_2, this.state.average.yuwen_1,this.state.average.sum],
+          tooltip: {
+            trigger: "axis",
+            axisPointer: {
+              type: "shadow",
+            },
           },
-          {
-            name: "个人",
-            type: "bar",
-            data: [this.state.single.yuwen_4, this.state.single.yuwen_3, this.state.single.yuwen2, this.state.single.yuwen_1,this.state.single.sum],
+          legend: {
+            data: ["总体", "个人"],
           },
-        ],
-      });
-
+          grid: {
+            left: "3%",
+            right: "4%",
+            bottom: "3%",
+            containLabel: true,
+          },
+          xAxis: {
+            type: "value",
+            boundaryGap: [0, 0.01],
+          },
+          yAxis: {
+            type: "category",
+            data: ["yuwen_4", "yuwen_3", "yuwen_2", "yuwen_1", "总分"],
+          },
+          series: [
+            {
+              name: "总体",
+              type: "bar",
+              data: [
+                this.state.average.yuwen_4,
+                this.state.average.yuwen_3,
+                this.state.average.yuwen_2,
+                this.state.average.yuwen_1,
+                this.state.average.sum,
+              ],
+            },
+            {
+              name: "个人",
+              type: "bar",
+              data: [
+                this.state.single.yuwen_4,
+                this.state.single.yuwen_3,
+                this.state.single.yuwen2,
+                this.state.single.yuwen_1,
+                this.state.single.sum,
+              ],
+            },
+          ],
+        });
       }
-    })
+    });
   };
 
   onInput = (value) => {
     this.setState({
       studentId: value.target.value,
     });
-    console.log(value.target.value);
+  };
+
+  onPdf = () => {
+    const input = document.getElementById("charts");
+
+    html2canvas(input).then((canvas) => {
+      const imgData = canvas.toDataURL("image/png");
+      const pdf = new jsPDF("p", "px", "a4");
+      var pdfWidth = pdf.internal.pageSize.getWidth();
+      var pdfHeight = pdf.internal.pageSize.getHeight();
+
+      var imgWidth = pdfWidth - 10;
+      var imgHeight = (imgWidth * canvas.height) / canvas.width;
+      var startX = 10 / 2;
+      var startY = (pdfHeight - imgHeight) / 2;
+      console.log(imgWidth);
+      console.log(imgHeight);
+      console.log(startX);
+      console.log(startY);
+
+      pdf.addImage(imgData, "PNG", startX, startY, imgWidth, imgHeight);
+      // pdf.addImage(imgData, "JPEG", 0, 0, 200, (200 * height) / width);
+      pdf.save("test.pdf");
+    });
   };
 
   render() {
@@ -197,13 +241,18 @@ class App extends Component {
         <Button type="primary" className="subButton" onClick={this.onSub}>
           查找
         </Button>
+        <Button type="primary" className="subButton" onClick={this.onPdf}>
+          Download Pdf
+        </Button>
         <div>{this.state.info}</div>
-
-        <div id="echarts" style={{ width: "100%", height: 600 }}></div>
-        <div
-          id="allStudentEcharts"
-          style={{ width: "100%", height: 600 }}
-        ></div>
+        <div id="charts">
+          <div style={{ fontSize: "50px" }}>报告结果</div>
+          <div id="echarts" style={{ width: "100%", height: 600 }}></div>
+          <div
+            id="allStudentEcharts"
+            style={{ width: "100%", height: 600 }}
+          ></div>
+        </div>
       </div>
     );
   }
